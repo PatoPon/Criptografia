057800b7bb6e83a7d51ca33fb96db6b039fd602b
PatoPon <novaesgui2323@outlook.com>
2023-09-10 17:40:25 -0300

    Another one

--- main.py
+++ main.py
@@ -3,79 +3,56 @@ def pegar_bytes(x):
     cha = bytes(x, 'utf-8')
     return cha
 
-
 def geradorconlinear(num_aleatorios, x_0, modulo, multiplicador, incremento):
-
     num_aleatorios[0] = x_0
-
     for i in range(1, len(num_aleatorios)):
         num_aleatorios[i] = (num_aleatorios[i - 1] *
                              multiplicador + incremento) % modulo
 
-
-def soma_bytes(byte1, byte2):
-    return byte1+byte2
-
-msg = input("Entre com a mensagem: ")
-
-while True:
-
-    chave = input("Entre com a chave de até 128 caracteres: ")
-    
-    # Verifica o comprimento da chave
-    if len(chave) <= 128:
-        break  # A chave tem o comprimento desejado, sai do loop
-    else:
-        print("A chave excede o limite de 128 caracteres. Tente de novo")
-
-while len(chave) < len(msg):
-    chave += chave
-
-# -- Variáveis para o gerador congruente linear (GCL) --
-
-# Quantidade de caracteres
+# Define as constantes do gerador
 quan = 5000
-
-# Quantidade de números pseudo-aleatórios
 num_aleatorios = [0] * quan
-
-# Valor inicial
 x_0 = 4
-
-# Módulo (choose a large prime number)
 modulo = 256
-
-# Multiplicador (choose a prime number)
 multiplicador = 133
-
-# Incremento (choose a prime number)
 incremento = 23
-
-# -- Fim variaveis GCL --
 
 geradorconlinear(num_aleatorios, x_0, modulo, multiplicador, incremento)
 
-chave = pegar_bytes(chave)
+def criptografar(msg, chave):
+    # Garante que a chave tenha o mesmo comprimento da mensagem ou mais
+    while len(chave) < len(msg):
+        chave += chave
+    
+    chave = pegar_bytes(chave)
+    msg_byte = pegar_bytes(msg)
+    
+    chave_bytes = [byte for byte in chave]
+    keystream_bits = [byte for byte in num_aleatorios]
+    
+    mensagem_cifrada = bytes(bit_msg ^ bit_chave ^ bit_keystream for (bit_msg, bit_chave, bit_keystream) in zip(msg_byte, chave_bytes, keystream_bits))
+    
+    return mensagem_cifrada
 
-# Cria um set sem repetições
-msg_byte = pegar_bytes(msg)
 
-chave_bytes = []
+def descriptografar(mensagem_cifrada, chave):
+    while len(chave) < len(mensagem_cifrada):
+        chave += chave
+    
+    chave = pegar_bytes(chave)
+    
+    chave_bytes = [byte for byte in chave]
+    keystream_bits = [byte for byte in num_aleatorios]
+    
+    dados_descriptografados = bytes(bit_dado ^ bit_chave ^ bit_keystream for (bit_dado, bit_chave, bit_keystream) in zip(mensagem_cifrada, chave_bytes, keystream_bits))
+    
+    return dados_descriptografados.decode('utf-8')
 
-for byte in chave:
-    chave_bytes.append(byte)
+mensagem_original = input("Qual a mensagem? ")
+chave = input("Qual a chave? ")
 
-keystream_bits = []
-
-for byte in num_aleatorios:
-    keystream_bits.append(byte)
-
-print(chave_bytes)
-print(keystream_bits)
-
-mensagem_cifrada = bytes(bit_msg ^ bit_chave ^ bit_keystream for (bit_msg, bit_chave, bit_keystream) in zip(msg_byte, chave_bytes, keystream_bits))
-dados_descriptografados = bytes(bit_dado ^ bit_chave ^ bit_keystream for (bit_dado, bit_chave, bit_keystream) in zip(mensagem_cifrada, chave_bytes, keystream_bits))
-
-# A variável 'mensagem_original' agora deve conter a mensagem original
+mensagem_cifrada = criptografar(mensagem_original, chave)
 print("Mensagem criptografada:", mensagem_cifrada)
-print("Mensagem descriptografada:", dados_descriptografados.decode('utf-8'))
+
+dados_descriptografados = descriptografar(mensagem_cifrada, chave)
+print("Mensagem descriptografada:", dados_descriptografados)

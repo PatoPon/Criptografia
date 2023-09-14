ce157c46d05dd0d817d5a47d86024df351496b7d
PatoPon <novaesgui2323@outlook.com>
2023-09-14 16:41:59 -0300

    Melhor criptografia

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 # A ideia original era usar a AES mas ela é muito complexa e ficaria mais dificil tanto de
 # escrever como explicar
 
-# A cifra de fluxo se baseia em usar chaves bits originais combinados com uma corrente de 
-# bits de cifragem (chamados de fluxo de chave) vindos de um gerador de dígitos pseudo-aleatório (GCL nesse 
+# A cifra de fluxo se baseia em usar chaves bits originais combinados com uma corrente de
+# bits de cifragem (chamados de fluxo de chave) vindos de um gerador de dígitos pseudo-aleatório (GCL nesse
 # caso). Normalmente esta combinação é feita através de uma operação de disjunção exclusiva ou XOR.
 
 # Foi usado o XOR por utilizar algo que eu me familiarizei nas aulas da Priscila sobre circuitos.
@@ -16,9 +16,11 @@
 # Únicas bibliotecas que eu resolvi usar. Somente pra mostrar uma interface e o tempo que passou
 import tkinter as tk
 import time as ti
+import hashlib
 
 # Função pra criar um botão "Copiar"
 copiar_button_chave = None
+
 
 def criar_botao_copiar(texto, rotulo):
     copiar_button = tk.Button(
@@ -28,6 +30,7 @@ def criar_botao_copiar(texto, rotulo):
 
 # Função para criptografar
 
+
 def criptografar():
 
     com_tempo = ti.time()
@@ -35,11 +38,18 @@ def criptografar():
     mensagem = mensagem_entry.get()
     chave = chave_entry.get()
 
-    while len(chave) < len(mensagem):
-        chave += chave
+    key_binary = hashlib.sha256(chave.encode('utf-8')).digest()
+    key_binary = key_binary[:16]
+
+    new_keystream = (keystream_bits[-1] * multiplicador + incremento) % modulo
+    keystream_bits.append(new_keystream)
+
+    encrypted_key = bytes(
+        bit_user_key ^ bit_keystream for (bit_user_key, bit_keystream) in zip(key_binary, keystream_bits)
+    )
 
     mensagem_cifrada = bytes(
-        bit_msg ^ bit_chave ^ bit_keystream for (bit_msg, bit_chave, bit_keystream) in zip(mensagem.encode('utf-8'), chave.encode('utf-8'), keystream_bits)
+        bit_msg ^ encrypted_key for (bit_msg, encrypted_key) in zip(mensagem.encode('utf-8'), encrypted_key)
     )
 
     tempo_final = ti.time()  # Registra o tempo final
@@ -58,6 +68,7 @@ def criptografar():
 
 # Função para descriptografar
 
+
 def descriptografar():
 
     com_tempo = ti.time()
@@ -65,12 +76,16 @@ def descriptografar():
     mensagem_cifrada = mensagem_cifrada_entry.get()
     chave = chave_descriptografia_entry.get()
 
-    while len(chave) < len(mensagem_cifrada):
-        chave += chave
+    key_binary = hashlib.sha256(chave.encode('utf-8')).digest()
+    key_binary = key_binary[:16]
+
+    encrypted_key = bytes(
+        bit_user_key ^ bit_keystream for (bit_user_key, bit_keystream) in zip(key_binary, keystream_bits)
+    )
 
     mensagem_original = bytes(
-        bit_dado ^ bit_chave ^ bit_keystream for (bit_dado, bit_chave, bit_keystream) in zip(bytes.fromhex(mensagem_cifrada), chave.encode('utf-8'), keystream_bits)
-    ).decode('utf-8')
+        bit_dado ^ bit_chave for (bit_dado, bit_chave) in zip(bytes.fromhex(mensagem_cifrada), encrypted_key)
+    ).decode('utf-8', errors="ignore")
 
     tempo_final = ti.time()  # Registra o tempo final
     tempo_total = tempo_final - com_tempo  # Calcula o tempo decorrido
@@ -97,14 +112,15 @@ root = tk.Tk()
 root.title("Criptografia e Descriptografia (Cifra de fluxo)")
 
 # Define a largura e a altura da janela (A default é muito pequena)
-largura = 500 
+largura = 500
 altura = 400
 root.geometry(f"{largura}x{altura}")
 
 # Criação dos elementos da interface
 
 # Meu nome!!!!
-seu_nome_label = tk.Label(root, text="Guilherme Novaes Campos - Trabalho do Primeiro Periodo", fg="gray")
+seu_nome_label = tk.Label(
+    root, text="Guilherme Novaes Campos - Trabalho do Primeiro Periodo", fg="gray")
 seu_nome_label.pack(side="bottom", padx=10, pady=10)
 
 # Rótulo e campo de entrada pra mensagem ser criptografada
@@ -166,7 +182,7 @@ resultado_descriptografia.pack()
 # Variáveis pro gerador congruente linear (Essa parte que eu mais demorei pra entender)
 # Basicamente é uma equação que o programa vai usar pra criar números pseudo-aleatorios
 
-quan = 5000
+quan = 128
 num_aleatorios = [0] * quan
 x_0 = 4
 modulo = 256
@@ -197,5 +213,5 @@ root.mainloop()
 # http://uab.ifsul.edu.br/tsiad/conteudo/modulo5/src/biblioteca/1_Leitura_Complementar_Algoritmos_de_Criptografia.pdf.pdf
 # https://institucional.upis.br/biblioteca/pdf/revistas/revista_informatica/Cavalcante_teoria_numeros_criptografia_2005_UPIS.pdf
 #
-# Braga, Alexandre, and Ricardo Dahab. "Introdução à criptografia para programadores: Evitando maus usos da criptografia em sistemas de 
+# Braga, Alexandre, and Ricardo Dahab. "Introdução à criptografia para programadores: Evitando maus usos da criptografia em sistemas de
 # software." Sociedade Brasileira de Computação (2015).
